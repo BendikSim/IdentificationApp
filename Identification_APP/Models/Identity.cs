3ea5e2703d0c94398fd673add642984da19a8681
BendikSim <Bendiksim98@gmail.com>
2021-04-24 16:53:00 +0200

    Add project files.

---
+++ Identification_APP/Models/Identity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identification_APP.Models
+{
+    public class Identity
+    {
+        public string providerId { get; set; }
+        public string fullName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string dataOfBirth { get; set; }
+
+    }
+}
